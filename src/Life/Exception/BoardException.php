c2a3ec7bad17282cb2ade079dc24d24456421830
Shane Auckland <shane.auckland@gmail.com>
2013-02-21 15:04:02 +0000

    updating exceptions

--- src/Life/Exception/BoardException.php
+++ src/Life/Exception/BoardException.php
@@ -14,18 +14,7 @@ namespace Life\Exception;
  * Board exception
  */
 
-class BoardException extends \Exception
+class BoardException extends LifeException
 {
-    /**
-     * Customer constructor removes the need to specify a code argument
-     *
-     * @param string $message Exception message
-     * @param \Exception $previous Previos exception for exception chaining
-     *
-     * @return void
-     */
-    public function __construct($message, \Exception $previous = null)
-    {
-        parent::__construct($message, 0, $previous);
-    }
+    // empty
 }
